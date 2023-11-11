5199a17d492d162c31d22ce3553efcf20e01fff6
Vlad Chmara <chmaravlad5@gmail.com>
2023-11-11 11:10:04 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    app.listen(PORT, () => {
+      console.log(`App is listening on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log('errror in main file');
+  }
+};
+
+start();
