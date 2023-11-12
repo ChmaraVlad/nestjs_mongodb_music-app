adeb15cdeff5e0b7f997f5fbbba5d85b69d403a6
Vlad Chmara <chmaravlad5@gmail.com>
2023-11-12 19:16:49 +0200

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
     app.listen(PORT, () => {
       console.log(`App is listening on PORT ${PORT}`);
     });
